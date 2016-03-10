babfdf44247ac537164501153f93121c9ad954a9
Cathy Zhu <cathyxz@gmail.com>
2016-03-10 14:04:30 -0800

    audio plays when you see moon

---
+++ OculusXBox/Assets/Scripts/PlayClipOnLook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayClipOnLook : MonoBehaviour {
+
+    private bool seen = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	    if(gameObject.GetComponent<Renderer>().isVisible && !seen)
+        {
+            seen = true;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+	}
+}
